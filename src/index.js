eb567c48fadf38ff7e2a605cfd7f7387b5f63e50
hon2a <jkonopasek@wiser.com>
2019-06-14 14:15:48 +0200

    Improved logging slightly (added start and end messages, muted internal commands).

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-export const without = (callback, options) =>
-  cy
-    .document(options)
-    .its('documentElement')
-    .within(callback, options)
+export const without = (callback, { log: shouldLog = true, ...options } = {}) => {
+  const internalOptions = { ...options, log: false }
+  const log = message => () => shouldLog && Cypress.log({ name: 'without', message })
+  cy.wrap(undefined, { log: false })
+    .then(log('START'))
+    .then(() => cy.document(internalOptions))
+    .then(({ documentElement }) => documentElement)
+    .within(internalOptions, callback)
+    .then(log('END'))
+}
