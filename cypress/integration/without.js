0bcebf2a863584328a621ebb25a8c610ea1f4778
hon2a <jkonopasek@wiser.com>
2019-06-14 13:54:32 +0200

    Slightly improved tests and changed them to import the command directly.

--- cypress/integration/without.js
+++ cypress/integration/without.js
@@ -1,3 +1,5 @@
+import { without } from '../../src'
+
 describe('without', () => {
   beforeEach(() => {
     cy.visit('cypress/fixtures/index.html')
@@ -5,21 +7,27 @@ describe('without', () => {
 
   it('breaks out of a `within`', () => {
     cy.get('#first').within(() => {
+      // only select the link inside the paragraph
       cy.get('a')
         .should('have.lengthOf', 1)
         .and('have.text', 'first link')
-      cy.without($root => {
+
+      // select all links inside a `without`
+      without($root => {
         cy.document().should(({ documentElement }) => expect($root[0]).to.equal(documentElement))
         cy.get('a')
           .should('have.lengthOf', 2)
           .eq(1)
           .should('have.text', 'second link')
       })
+
+      // continue correctly `within` the previous container (select just the one link again)
+      cy.get('a').should('have.lengthOf', 1)
     })
   })
 
   it('works outside of a `within` (but does nothing interesting)', () => {
-    cy.without($absoluteRoot => {
+    without($absoluteRoot => {
       cy.root().should($localRoot => expect($absoluteRoot[0]).to.equal($localRoot[0]))
       cy.get('a')
         .should('have.lengthOf', 2)
